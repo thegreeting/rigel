83b0f0250d5e159863e0f44be07ebe9bf2659ed3
Kenichi Naoe <ken@naoe.tokyo>
2022-11-05 13:09:29 -0700

    :sparkles: add script for XCode Cloud

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Runner
+#
+#  Created by Kenichi Naoe on 2022/02/18.
+#  
+flutter_version=`cat ../../.tool-versions | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+(-([0-9]+\.)*[0-9a-zA-Z]+)?).*/\1/p' | sed 's/-stable//'`
+echo "Flutter version: `echo $flutter_version`"
+
+echo "🔵 install cocoapods"
+if ! command -v brew &> /dev/null
+then
+    echo "🔵 brew could not be found. installing brew..."
+    mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+    export PATH=$PATH:homebrew/bin
+fi
+
+brew install cocoapods
+
+echo "🔵 flutter download"
+git clone https://github.com/flutter/flutter.git -b $flutter_version ~/dev/flutter
+
+export PATH="$PATH:~/dev/flutter/bin"
+
+echo "🔵 flutter setup"
+flutter doctor
+flutter pub get
+flutter precache --ios
+
+echo "🔵 pod install"
+pod install
